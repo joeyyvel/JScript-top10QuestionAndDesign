4893ac9870b5760ea872e887fdff4960b832f55b
joeyboi <66668752+joeyyvel@users.noreply.github.com>
2024-11-20 10:36:25 +0800

    adding duplicate method

--- script.js
+++ script.js
@@ -15,27 +15,44 @@
 /*----------------------------------------------------------------------*/
 /*-------------------------LONGEST WORD---------------------------------*/
 
-function findLongestWord(sentence) {
-  let words = sentence.split(" ");
-  let longestWord = "";
+// function findLongestWord(sentence) {
+//   let words = sentence.split(" ");
+//   let longestWord = "";
 
-  for (let word of words) {
-    if (word.length > longestWord.length) {
-      longestWord = word;
-    }
-  }
-  return longestWord;
-}
+//   for (let word of words) {
+//     if (word.length > longestWord.length) {
+//       longestWord = word;
+//     }
+//   }
+//   return longestWord;
+// }
 
-console.log(findLongestWord("i love javascript programming"));
+//console.log(findLongestWord("i love javascript programming"));
 
 /*-----------------------------------------------------------------------*/
 /*-------------------------PALINDROME----------------------------------- */
 
-function checkPalindrome(word) {
-  let stringOne = word.split("").reverse().join("");
+// function checkPalindrome(word) {
+//   let stringOne = word.split("").reverse().join("");
 
-  return word === stringOne;
+//   return word === stringOne;
+// }
+
+// console.log(checkPalindrome("kayak"));
+
+/*------------------------------------------------------------------------*/
+/*---------------------------DUPLICATE------------------------------------*/
+
+function removeDuplicate(num) {
+  let uniqueElements = [];
+
+  for (let i = 0; i < num.length; i++) {
+    if (uniqueElements.indexOf(num[i]) === -1) {
+      uniqueElements.push(num[i]);
+    }
+  }
+
+  return uniqueElements;
 }
 
-console.log(checkPalindrome("kayak"));
+console.log(removeDuplicate([1, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8, 8]));
