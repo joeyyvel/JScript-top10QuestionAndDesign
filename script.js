ce786458bb6ecb914402cd53c3d73bc901d8f227
joeyboi <66668752+joeyyvel@users.noreply.github.com>
2024-11-20 10:19:28 +0800

    palindrome and longest word

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 /*------------------------REVERSE STRING------------------------------*/
 
-function reverseString(str) {
-  let reversedWord = [];
+// function reverseString(str) {
+//   let reversedWord = [];
 
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversedWord += str[i];
-  }
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversedWord += str[i];
+//   }
 
-  return reversedWord;
-}
+//   return reversedWord;
+// }
 
-console.log(reverseString("Hello javascript programming"));
+// console.log(reverseString("Hello javascript programming"));
 
 /*----------------------------------------------------------------------*/
+/*-------------------------LONGEST WORD---------------------------------*/
+
+function findLongestWord(sentence) {
+  let words = sentence.split(" ");
+  let longestWord = "";
+
+  for (let word of words) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+  return longestWord;
+}
+
+console.log(findLongestWord("i love javascript programming"));
+
+/*-----------------------------------------------------------------------*/
+/*-------------------------PALINDROME----------------------------------- */
+
+function checkPalindrome(word) {
+  let stringOne = word.split("").reverse().join("");
+
+  return word === stringOne;
+}
+
+console.log(checkPalindrome("kayak"));
